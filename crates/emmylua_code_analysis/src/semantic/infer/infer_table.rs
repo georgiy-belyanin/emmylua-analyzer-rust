cc498f50a086ee1e2052446b8e0924a113cf0b47
Georgiy Belyanin <belyaningeorge@ya.ru>
2025-02-07 11:47:04 +0300

    Infer table array consts as tuples

--- crates/emmylua_code_analysis/src/semantic/infer/infer_table.rs
+++ crates/emmylua_code_analysis/src/semantic/infer/infer_table.rs
@@ -1,12 +1,24 @@
 use emmylua_parser::{LuaAstNode, LuaTableExpr};
 
-use crate::db_index::{DbIndex, LuaType};
+use crate::db_index::{DbIndex, LuaType, LuaTupleType};
 
-use super::{InferResult, LuaInferConfig};
+use super::{InferResult, LuaInferConfig, infer_expr};
 
-pub fn infer_table_expr(_: &DbIndex, config: &LuaInferConfig, table: LuaTableExpr) -> InferResult {
-    Some(LuaType::TableConst(crate::InFiled {
-        file_id: config.get_file_id(),
-        value: table.get_range(),
-    }))
+pub fn infer_table_expr(db: &DbIndex, config: &mut LuaInferConfig, table: LuaTableExpr) -> InferResult {
+    if table.is_array() {
+        infer_table_array_expr(db, config, table)
+    } else {
+        Some(LuaType::TableConst(crate::InFiled {
+            file_id: config.get_file_id(),
+            value: table.get_range(),
+        }))
+    }
+}
+
+fn infer_table_array_expr(db: &DbIndex, config: &mut LuaInferConfig, table: LuaTableExpr) -> InferResult {
+    let field_types = table.get_fields()
+        .map(|field| infer_expr(db, config, field.get_value_expr()?))
+        .map(|field_type| field_type.unwrap_or(LuaType::Unknown))
+        .collect::<Vec<_>>();
+    Some(LuaTupleType::new(field_types).into())
 }
